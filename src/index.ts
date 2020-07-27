63fa9d2cf8bdf68f3730cce40fa98c4fdcecfa0b
Douglas Neves <d88.neves@gmail.com>
2020-07-26 21:58:51 -0300

    TypeScript
    TypeScript

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import { helloWorld } from './routes';
+
+const app = express();
+
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hellow World' });
+});
+
+app.listen(3333);
